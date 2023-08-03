3a4061df719d9f625cdd16f13eb6069676c92230
Dasom Park <dev.somda@gmail.com>
2023-08-03 18:25:19 +0900

    ✨ feature: Main Router 추가

--- src/pages/Routers.jsx
+++ src/pages/Routers.jsx
@@ -1,9 +1,11 @@
 import { Route, Routes } from "react-router-dom";
+import Main from "./Main";
 import SignUp from "./SignUp";
 
 export default function Routers() {
   return (
     <Routes>
+      <Route path="/" element={<Main />} />
       <Route path="/signup" element={<SignUp />} />
     </Routes>
   );
